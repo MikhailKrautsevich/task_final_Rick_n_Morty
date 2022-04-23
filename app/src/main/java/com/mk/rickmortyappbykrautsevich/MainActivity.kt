aa6333bf36db9d0f2263f1cce079c3deb99f2400
MikhailKrautsevich <mihail4egevara@gmail.com>
2022-04-23 21:07:19 +0300

    initial commit

---
+++ app/src/main/java/com/mk/rickmortyappbykrautsevich/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mk.rickmortyappbykrautsevich
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
